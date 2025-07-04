5f4b675a3f56387fd8689164ffbd410ecbbf76e1
P.Binh (ビン) <binhphan.dev@gmail.com>
2025-07-05 00:06:52 +0700

    chore: update site config

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Teasersoftware",
+  name: "Teaser EBook",
   description:
-    "Nền tảng E-Learning tương tác với PDF Viewer tiên tiến. Hỗ trợ đọc PDF tương tác, flipbook, zoom, fullscreen và nhiều tính năng khác.",
+    "Nền tảng E-Learning tương tác với PDF Viewer tiên tiến được phát triển bởi Teaser. Hỗ trợ đọc PDF tương tác, flipbook, zoom, fullscreen và nhiều tính năng khác.",
   mainNav: [],
   links: {},
 }
